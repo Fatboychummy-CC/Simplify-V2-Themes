cc208be09daf08ef3d89c021477c37fe7f558e4d
Matthew W <fatboychummy@gmail.com>
2021-03-09 18:27:07 -0700

    parse them badbois

--- Themes.lua
+++ Themes.lua
@@ -17,23 +17,88 @@ end
 
 -- parse the custom CSV format.
 local function parseCustomCSV(data)
+  expect(1, data, "string")
 
+  local parsed = {}
+
+  -- clear out whitespace
+  data = data:gsub(' ', "")
+
+  -- clear out comments
+  data = data:gsub('#.-\n', "\n")
+
+  -- basic crappy error handling
+  if data:find(",,") or data:find(",\n") then
+    local i = 1
+    for line in data:gmatch("[^\n]+") do
+      if data:find(",,") or data:find(",\n") then
+        error(string.format("CSV syntax error on line %d of input.", i))
+      end
+      i = i + 1
+    end
+  end
+
+  -- process each line that has data in it.
+  local i = 1
+  for line in data:gmatch("[^\n]+") do
+    local index = 1
+    local items = {}
+    local category, subcategory
+
+    -- process each comma seperated value
+    for label in line:gmatch("[^,]+") do
+      if index == 1 then
+        category = label
+      elseif index == 2 then
+        subcategory = label
+      else
+        items[#items + 1] = label
+      end
+      index = index + 1
+    end
+
+    -- basic error check for missing data
+    if not category or not subcategory or #items == 0 then
+      error(string.format("CSV missing data on line %d of input.", i))
+    end
+
+    -- add to parsed data
+    parsed[#parsed + 1] = {
+      category = category,
+      subcategory = subcategory,
+      items = items
+    }
+
+    i = i + 1
+  end
+
+  return parsed
 end
 
 local module = {
   sources = {
     themeList = "https://raw.githubusercontent.com/Fatboychummy-CC/Simplify-V2-Themes/main/List.csv",
     layoutList = "https://raw.githubusercontent.com/Fatboychummy-CC/Simplify-V2-Themes/main/Layout.csv"
-    self = "https://raw.githubusercontent.com/Fatboychummy-CC/Simplify-V2-Themes/main/Themes.lua"
+    self = "https://raw.githubusercontent.com/Fatboychummy-CC/Simplify-V2-Themes/main/Themes.lua",
+    rootTheme = "https://raw.githubusercontent.com/Fatboychummy-CC/Simplify-V2-Themes/main/Themes/",
+    rootLayouts = "https://raw.githubusercontent.com/Fatboychummy-CC/Simplify-V2-Themes/main/Layouts/"
   }
 }
 
 function module.getThemes()
+  return parseCustomCSV(getFile(module.sources.themeList))
+end
+
+function module.getTheme(category, subcategory, label)
 
 end
 
 function module.getLayouts()
+  return parseCustomCSV(getFile(module.sources.layoutList))
+end
 
+function module.getLayout(category, subcategory, label)
+  
 end
 
 return module
